e9f3de170f50ba3b1bd8ff58cbf0081456d0eae0
eher78 <abrahamschaecher@outlook.com>
2023-03-07 23:24:48 -0600

    first commit

---
+++ lessons/lesson2/main.cpp
@@ -0,0 +1,16 @@
+
+// This brings in the iostream library
+#include <iostream>
+
+/* 
+This is a multithread comment
+*/
+
+int main(){
+    // This will print "Hello World" 10 times
+    std::cout << "Hello Abraham" << std::endl;
+    std::cout << "Hello Abraham" << std::endl;
+
+    return 0;
+    // Program ends here
+}

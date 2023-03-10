c70624a460e311664da09ba312b552d07ee4f3e5
eher78 <abrahamschaecher@outlook.com>
2023-03-09 23:25:19 -0600

    Added files

---
+++ lessons/lesson3/numberSystems.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    
+    int number1 = 15; // Decimal
+    int number2 = 017; // Octal
+    int number3 = 0x0F; // Hexadecimal
+    int number4 = 0b0000111; // Binary
+
+    std::cout << "number1: " << number1 << std::endl;
+    std::cout << "number2: " << number1 << std::endl;
+    std::cout << "number3: " << number1 << std::endl;
+    std::cout << "number4: " << number1 << std::endl;
+
+    return 0;
+}

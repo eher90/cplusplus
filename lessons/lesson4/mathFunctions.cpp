0cd85f447442fcac909c8447af7f257e16bce258
eher78 <abrahamschaecher@outlook.com>
2023-03-12 15:43:40 -0500

    Added math functions

---
+++ lessons/lesson4/mathFunctions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+    double weight = 7.7;
+    
+    // floor 
+    std::cout << "Weight rounded to floor is: " << std::floor(weight) << std::endl;
+
+    // ceil
+    std::cout << "Weight rounded to ceiling is: " << std::ceil(weight) << std::endl;
+
+    //abs
+    double savings = -5000;
+
+    std::cout << "Abs of weight is: " << std::abs(weight) << std::endl;
+    std::cout << "Abs of savings is: " << std::abs(savings) << std::endl;
+
+    // exp: f(x) = e^x
+    double exponential = std::exp(10);
+    std::cout << "The exponential of 10 is: " << exponential << std::endl;
+
+    // pow
+    std::cout << "3^4 = " << std::pow(3, 4) << std::endl;
+    std::cout << "9^3 = " << std::pow(9, 3) << std::endl;
+
+    return 0;
+}

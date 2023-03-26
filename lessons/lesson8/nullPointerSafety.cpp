db6511acb2a544d61fed0ded83da4e3dcf4609dd
eher78 <abrahamschaecher@outlook.com>
2023-03-26 00:26:56 -0500

    Added null pointer safety checks.

---
+++ lessons/lesson8/nullPointerSafety.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+    
+    // Verbose nullptr check
+    std::cout << std::endl;
+    std::cout << "Verbose nullptr check: " << std::endl;
+
+    int *p_number{};
+    p_number = new int(7);
+
+    if (!(p_number == nullptr)){
+        std::cout << "p_number points to a VALID address: " << p_number << std::endl;
+        std::cout << "*p_number: " << *p_number << std::endl;
+    } else {
+        std::cout << "p_number points to an INVALID address." << std::endl;
+    }
+
+    delete p_number;
+    nullptr;
+
+
+    return 0;
+}

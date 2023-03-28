c854fb93d3d3649507b2144a219c4036d2ee1939
eher78 <abrahamschaecher@outlook.com>
2023-03-27 19:35:37 -0500

    Added intro to references.

---
+++ lessons/lesson9/usingReferences.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(){
+
+    int int_data = 33;
+    double double_data = 55;
+
+    // References
+    int& ref_int_data = int_data;
+    double& ref_double_data = double_data;
+
+    // Print stuff out
+    std::cout << "int_data: " << int_data << std::endl;
+    std::cout << "&int_data: " << &int_data << std::endl;
+    std::cout << "double_data: " << double_data << std::endl;
+    std::cout << "&double_data: " << &double_data << std::endl;
+
+    std::cout << "===================================" << std::endl;
+
+    std::cout << "ref_int_data: " << ref_int_data << std::endl;
+    std::cout << "&ref_int_data: " << &ref_int_data << std::endl;
+    std::cout << "ref_double_data: " << ref_double_data << std::endl;
+    std::cout << "&ref_double_data: " << &ref_double_data << std::endl;
+
+    return 0;
+}

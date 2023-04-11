06bdd7649a29c2e71343afa8ed93fd149998c318
eher78 <abrahamschaecher@outlook.com>
2023-04-10 20:01:32 -0500

    Added template specialization lesson

---
+++ lessons/lesson15/templateSpecialization.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+template <typename T> T maximum(T a, T b){
+    return (a > b)? a : b;
+}
+
+template <>
+const char* maximum<const char*> (const char* a, const char* b){
+    return (std::strcmp(a, b) > 0)? a : b;
+}
+
+int main(){
+
+    int a = 10;
+    int b = 23;
+    double c = 34.7;
+    double d = 23.4;
+    std::string e = "Hello";
+    std::string f = "World";
+
+    auto max_int = maximum(a, b); // int type deduced
+    auto max_double = maximum(c, d); // double type deduced
+    auto max_str = maximum(e, f); // string type deduced
+
+    std::cout << "max_int: " << max_int << std::endl;
+    std::cout << "max_double: " << max_double << std::endl;
+    std::cout << "max_str: " << max_str << std::endl;
+
+    // 
+    const char* g = "wild";
+    const char* h = "animal";
+
+    // This wont do what you expect: BEWARE!
+    const char* result = maximum(g, h);
+    std::cout << "max(const char*): " << result << std::endl;
+    
+    return 0;
+}
